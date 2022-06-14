7c844a45439d786645eef54bd6f6f73543b05eb8
madhav2k21 <madhav.anupoju.2k21@gmail.com>
2022-06-14 23:06:23 +0530

    adds testUserWithViolations

--- src/test/java/com/techleads/app/model/UsersTest.java
+++ src/test/java/com/techleads/app/model/UsersTest.java
@@ -1,8 +1,10 @@
 package com.techleads.app.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.Path;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UsersTest {
@@ -23,6 +26,30 @@ public class UsersTest {
         Iterator<ConstraintViolation<Users>> itr = violations.iterator();
         assertTrue(violations.isEmpty());
 
+    }
+    
+
+    @Test
+    public void testUserWithViolations() {
+        Users user = buildUser();
+        user.setLocation("");
+        Set<ConstraintViolation<Users>> violations = validator.validate(user);
+        assertEquals(2, violations.size());
+        Iterator<ConstraintViolation<Users>> iterator = violations.iterator();
+
+        assertEquals("location",violations.iterator().next().getPropertyPath().toString());
+
+//        System.out.println(violations.iterator().next());
+
+
+
+
+
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next().getMessage());
+//        }
+//        System.out.println("{} "+violations.iterator().next().getMessage());
+//        assertEquals("",violations.iterator().next().getMessage());
     }
 
 	public Users buildUser() {
