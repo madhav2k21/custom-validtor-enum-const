629d383852c5dfeb60bb352ee3bdf0af85a42829
madhav2k21 <madhav.anupoju.2k21@gmail.com>
2022-06-15 15:07:51 +0530

    ParameterizedTest

--- src/test/java/com/techleads/app/model/UsersTest.java
+++ src/test/java/com/techleads/app/model/UsersTest.java
@@ -1,17 +1,22 @@
 package com.techleads.app.model;
 
-import org.junit.jupiter.api.Assertions;
+import org.apache.logging.log4j.util.Strings;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.Path;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -28,37 +33,90 @@ public class UsersTest {
 
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"test",})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input) {
+//        assertTrue(Strings.isBlank(input));
+        assertTrue(Strings.isNotBlank(input));
+    }
 
-    @Test
-    public void testUserWith_location_Empty_Violations() {
+    @ParameterizedTest(name = "#{index} - Run test with args={0}")
+    @NullSource
+    @ValueSource(strings = {"", "  "})
+    public void testUserWith_location_Empty_Violations(String value) {
         Users user = buildUser();
 
-        user.setLocation(null);
+        user.setLocation(value);
 
         Set<ConstraintViolation<Users>> violations = validator.validate(user);
         assertEquals(2, violations.size());
 
         List<ConstraintViolation<Users>> constraintViolations = violations.stream()
-                .sorted((a,b)-> a.getMessage().compareTo(b.getMessage()))
+                .sorted((a, b) -> a.getMessage().compareTo(b.getMessage()))
                 .toList();
 
         assertEquals("location", constraintViolations.get(0).getPropertyPath().toString());
         assertEquals("Location field must not be empty", constraintViolations.get(0).getMessage());
         assertEquals("Location must be of HYD/CHN/BLR", constraintViolations.get(1).getMessage());
     }
-    @Test
-    public void testUserWith_location_WrongValue_Violations() {
+
+
+    @ParameterizedTest(name = "#{index} - Run test with args={0}")
+    @MethodSource("blankOrNullStrings")
+    public void testUserWith_location_Null_Violations(String input) {
         Users user = buildUser();
 
-        user.setLocation("test");
+        user.setLocation(input);
+
+        Set<ConstraintViolation<Users>> violations = validator.validate(user);
+        assertEquals(2, violations.size());
+
+        List<ConstraintViolation<Users>> constraintViolations = violations.stream()
+                .sorted((a, b) -> a.getMessage().compareTo(b.getMessage()))
+                .toList();
+
+        assertEquals("location", constraintViolations.get(0).getPropertyPath().toString());
+        assertEquals("Location field must not be empty", constraintViolations.get(0).getMessage());
+        assertEquals("Location must be of HYD/CHN/BLR", constraintViolations.get(1).getMessage());
+    }
+
+    static Stream<String> blankOrNullStrings() {
+        return Stream.of("", " ", null);
+    }
+    static Stream<String> acceptedLocatonCodes() {
+        return Stream.of("HYD_CDE", "BLR_CDE", "CHN_CDE");
+    }
+
+    @ParameterizedTest(name = "#{index} - Run test with args={0}")
+    @ValueSource(strings = {"madhav", "dill","test"})
+    public void testUserWith_location_InvalidValues_Violations(String customInput) {
+        Users user = buildUser();
+
+        user.setLocation(customInput);
 
         Set<ConstraintViolation<Users>> violations = validator.validate(user);
         assertEquals(1, violations.size());
 
         List<ConstraintViolation<Users>> constraintViolations = violations.stream().toList();
+        assertThat(violations.size()).isEqualTo(1);
 
         assertEquals("location", constraintViolations.get(0).getPropertyPath().toString());
         assertEquals("Location must be of HYD/CHN/BLR", constraintViolations.get(0).getMessage().toString());
+    }
+
+    @ParameterizedTest(name = "#{index} - Run test with args={0}")
+//    @ValueSource(strings = {"HYD_CDE", "BLR_CDE","CHN_CDE"})
+    @MethodSource("acceptedLocatonCodes")
+    public void testUserWith_location_AcceptedValues_Violations(String customInput) {
+        Users user = buildUser();
+
+        user.setLocation(customInput);
+
+        Set<ConstraintViolation<Users>> violations = validator.validate(user);
+        assertEquals(0, violations.size());
+        assertThat(violations.isEmpty()).isTrue();
+
+
     }
 
     public Users buildUser() {
